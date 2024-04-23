1c5123a2fd329d7dc8baf4ee4691849ffb2d43e4
sahin52 <sahinkasap52@gmail.com>
2024-04-23 16:35:33 +0300

    Getting input from console indefinitely is done

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
 import * as readline from 'readline';
-import { TranslateSentence } from './translator'
+import { NumberWordsToNumeric } from './NumberWordsToNumeric'
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
-});
-rl.question('Write a sentence to translate:', (answer) => {
-    
-    TranslateSentence(answer)
-
-    rl.close();
-});
-
-TranslateSentence("Test two")
+  });
+  
+  console.log('Welcome! Type your input to convert numbers as words into integers. Press Ctrl+C to exit.');
+  
+  const processInput = (input: string) => {
+    NumberWordsToNumeric(input);
+  };
+  
+  rl.on('line', (input) => {
+    processInput(input);
+  });
